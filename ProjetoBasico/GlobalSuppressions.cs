5b8f92445570b3e614897c3af737f0d2bb4491e7
FrancieleAires <f.aires2016@gmail.com>
2023-01-18 00:38:17 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoBasico/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0060:Remover o parâmetro não utilizado", Justification = "<Pendente>", Scope = "member", Target = "~M:FirstProject.Program.Main(System.String[])")]
+[assembly: SuppressMessage("Style", "IDE0059:Atribuição desnecessária de um valor", Justification = "<Pendente>", Scope = "member", Target = "~M:FirstProject.Program.Main(System.String[])")]
+[assembly: SuppressMessage("Usage", "CA2241:Fornecer os argumentos corretos para métodos de formatação", Justification = "<Pendente>", Scope = "member", Target = "~M:FirstProject.Program.Main(System.String[])")]
---
+++ ProjetoBasico/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double raio, area, pi = 3.14159;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = pi * raio * raio;
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
